a76d76f3bbf442b1f5f3a555f20946aca460d341
Yesi-Hernandez <yesenia1hernandez31@gmail.com>
2024-11-03 22:00:14 -0600

    Delete mi-sitio directory

--- mi-sitio/Pantalla2/JS/dist/funcionH.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function toggleMenu() {
-  var mobileMenu = document.querySelector('.mobile-menu');
-  mobileMenu.style.display = mobileMenu.style.display === 'flex' ? 'none' : 'flex';
-}
-//# sourceMappingURL=funcionH.dev.js.map
--- mi-sitio/Pantalla2/JS/funcionH.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const mobileMenu = document.querySelector('.mobile-menu');
-    mobileMenu.style.display = mobileMenu.style.display === 'flex' ? 'none' : 'flex';
-}
-
-function redirect() {
-    window.location.href = 'precios.html'; 
-}
